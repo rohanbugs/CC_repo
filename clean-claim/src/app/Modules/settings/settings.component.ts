29b5bb4893494fab2fc6152ff68903525c2cf8cc
rohanbugs <rohanpatil07ind@gmail.com>
2024-11-18 10:21:33 +0530

    selctionReasons

--- clean-claim/src/app/Modules/settings/settings.component.ts
+++ clean-claim/src/app/Modules/settings/settings.component.ts
@@ -1,15 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
-import { AccountComponent } from '../account/account.component';
-import { SecurityComponent } from '../security/security.component';
+import { AccountComponent } from '../settings/account/account.component';
+import { SecurityComponent } from '../settings/security/security.component';
 import { CommonModule } from '@angular/common';
 import { BreadcrumbComponent } from '../breadcrumb/breadcrumb.component';
 
 @Component({
   selector: 'app-settings',
   standalone: true,
-  imports: [RouterOutlet,AccountComponent,SecurityComponent,RouterModule,BreadcrumbComponent,CommonModule
-  ],
+  imports: [RouterOutlet,AccountComponent,SecurityComponent,RouterModule,BreadcrumbComponent,CommonModule],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.css'
 })
